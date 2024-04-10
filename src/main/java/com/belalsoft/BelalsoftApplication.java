7820af7c2522e086dadf195c5653e95626772295
Kamrujama Ansari <ansarikamrujama4@gmail.com>
2024-04-10 09:40:51 +0545

    initial commit

---
+++ src/main/java/com/belalsoft/BelalsoftApplication.java
@@ -0,0 +1,13 @@
+package com.belalsoft;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BelalsoftApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BelalsoftApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/belalsoft/BelalsoftApplicationTests.java
@@ -0,0 +1,13 @@
+package com.belalsoft;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BelalsoftApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
